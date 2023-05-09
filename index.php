e644c2a046628c42875ebabdaa2c8ea6aca5e790
Muhamad Rusydi Hakim Bin Muhamad Rizal <108201860+Rusydihakim@users.noreply.github.com>
2023-05-10 04:36:38 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,45 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Electricity Bill Calculator</title>
-	<!-- Include your preferred CSS framework here -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Electricity Bill Calculator</title>
+    <!-- Add Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Electricity Bill Calculator</h1>
-		<form method="post">
-			<div class="form-group">
-				<label for="voltage">Voltage (V)</label>
-				<input type="number" class="form-control" id="voltage" name="voltage" step="0.01" required>
-			</div>
-			<div class="form-group">
-				<label for="current">Current (A)</label>
-				<input type="number" class="form-control" id="current" name="current" step="0.01" required>
-			</div>
-			<div class="form-group">
-				<label for="rate">Current Rate (sen/kWh)</label>
-				<input type="number" class="form-control" id="rate" name="rate" step="0.01" required>
-			</div>
-			<button type="submit" class="btn btn-primary">Calculate</button>
-		</form>
-		<?php
-		if($_SERVER["REQUEST_METHOD"] == "POST") {
-			$voltage = $_POST["voltage"];
-			$current = $_POST["current"];
-			$rate = $_POST["rate"];
+    <div class="container">
+        <h1>Electricity Bill Calculator</h1>
+        <form method="post">
+            <div class="form-group">
+                <label for="voltage">Voltage (V)</label>
+                <input type="number" class="form-control" id="voltage" name="voltage" required>
+            </div>
+            <div class="form-group">
+                <label for="current">Current (A)</label>
+                <input type="number" step="0.01" class="form-control" id="current" name="current" required>
+            </div>
+            <div class="form-group">
+                <label for="hour">Hours Used</label>
+                <input type="number" class="form-control" id="hour" name="hour" required>
+            </div>
+            <div class="form-group">
+                <label for="rate">Current Rate (sen/kWh)</label>
+                <input type="number" step="0.01" class="form-control" id="rate" name="rate" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Calculate</button>
+        </form>
+        <?php
+            // Check if the form is submitted
+            if(isset($_POST['voltage']) && isset($_POST['current']) && isset($_POST['hour']) && isset($_POST['rate'])){
+                // Get the values from the form
+                $voltage = $_POST['voltage'];
+                $current = $_POST['current'];
+                $hour = $_POST['hour'];
+                $rate = $_POST['rate'];
 
-			$power = $voltage * $current;
-			$energy = $power * 1 * 1000; // assuming 1 hour
-			$total = $energy / 1000 * $rate; // convert to kWh
+                // Calculate the power, energy, and total charge
+                $power = $voltage * $current;
+                $energy = $power * $hour / 1000;
+                $total = $energy * $rate * 100;
 
-			echo "<h2>Results</h2>";
-			echo "<p>Power: " . $power . " W</p>";
-			echo "<p>Energy: " . $energy / 1000 . " kWh</p>";
-			echo "<p>Total Charge: $" . number_format($total, 2) . "</p>";
-		}
-		?>
-	</div>
-	<!-- Include your preferred JS framework here -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-KB6nhp3MIX3oIz+ZsXO/bW1s3qTZJ+ccTidr2F+67W8OJ/zmAHf6ViJX+QD0J8KvZGOGVOd1en0y+dxKjP8vA==" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha512-rcbQ2TXZ8zHuWl+TrIwduJ"></script>
+                // Display the result
+                echo '<div class="alert alert-success mt-3">';
+                echo '<p>Power: ' . $power . ' W</p>';
+                echo '<p>Energy: ' . $energy . ' kWh</p>';
+                echo '<p>Total Charge: ' . number_format($total, 2) . ' sen</p>';
+                echo '</div>';
+            }
+        ?>
+    </div>
+    <!-- Add Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
