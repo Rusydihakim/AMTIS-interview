cf583def6e329f188ad31310d9b1c5f536886785
Muhamad Rusydi Hakim Bin Muhamad Rizal <108201860+Rusydihakim@users.noreply.github.com>
2023-05-10 04:37:18 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Electricity Bill Calculator</title>
-    <!-- Add Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
@@ -50,7 +49,6 @@
             }
         ?>
     </div>
-    <!-- Add Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
